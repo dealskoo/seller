2e48d0e3f742aad81b74a94c47112afabc408ac2
jabber <2898117012@qq.com>
2021-12-31 23:15:54 +0800

    fix: expire

--- src/Notifications/ResetSellerPassword.php
+++ src/Notifications/ResetSellerPassword.php
@@ -19,7 +19,7 @@ class ResetSellerPassword extends ResetPassword
             ->subject(Lang::get('Reset Seller Password Notification'))
             ->line(Lang::get('You are receiving this email because we received a password reset request for your account.'))
             ->action(Lang::get('Reset Password'), $url)
-            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.' . config('auth.defaults.passwords') . '.expire')]))
+            ->line(Lang::get('This password reset link will expire in :count minutes.', ['count' => config('auth.passwords.sellers.expire')]))
             ->line(Lang::get('If you did not request a password reset, no further action is required.'));
     }
 
