77e12b8ac882c68593e4699dc5fcda3e91899108
jabber <2898117012@qq.com>
2022-01-10 01:59:58 +0800

    fix: notification

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8"/>
     <title>@yield('title') | {{ __('seller::auth.title') }} - {{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
     <!-- App css -->
---
+++ src/Notifications/EmailVerificationCodeNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Dealskoo\Seller\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EmailVerificationCodeNotification extends Notification
+{
+    use Queueable;
+
+    protected $verification_code;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($verification_code)
+    {
+        $this->verification_code = $verification_code;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param mixed $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param mixed $notifiable
+     * @return MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject(__('Email Verification code'))
+            ->line(__('Your verification code is: :code', ['code' => $this->verification_code]));
+    }
+}
