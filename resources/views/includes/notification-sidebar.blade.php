5d1417779c90aec0edec0f53f7e82bcf09539c36
jabber <2898117012@qq.com>
2022-01-11 02:11:52 +0800

    fix: notifications

---
+++ resources/views/includes/notification-sidebar.blade.php
@@ -0,0 +1,10 @@
+<!-- Left sidebar -->
+<div class="page-aside-left">
+    <div class="email-menu-list mt-3">
+        <a href="#" class="text-danger fw-bold"><i class="dripicons-inbox me-2"></i>{{ __('All') }}<span
+                class="badge badge-danger-lighten float-end ms-2">{{ Auth::user()->notifications()->count() }}</span></a>
+        <a href="#"><i class="dripicons-document me-2"></i>{{ __('Unread') }}<span
+                class="badge badge-info-lighten float-end ms-2">{{ Auth::user()->unreadNotifications()->count() }}</span></a>
+    </div>
+</div>
+<!-- End Left sidebar -->
