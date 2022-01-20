dc7f21779570885b0afb2c294200947633f9831e
jabber <2898117012@qq.com>
2022-01-20 16:02:23 +0800

    fix: test

---
+++ database/factories/SellerFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories\Dealskoo\Seller\Models;
+
+use Dealskoo\Seller\Models\Seller;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SellerFactory extends Factory
+{
+    protected $model = Seller::class;
+
+    public function definition()
+    {
+        return [];
+    }
+}
