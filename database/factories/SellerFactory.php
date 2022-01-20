a9d2cc67c6a37a56d69621fe126463fd0aa8b09a
jabber <2898117012@qq.com>
2022-01-20 18:33:03 +0800

    factory

--- database/factories/SellerFactory.php
+++ database/factories/SellerFactory.php
@@ -14,12 +14,17 @@ class SellerFactory extends Factory
     public function definition()
     {
         return [
+            'slug' => $this->faker->unique->slug(),
+            'avatar' => $this->faker->imageUrl(60, 60),
             'name' => $this->faker->name(),
+            'bio' => $this->faker->text(30),
             'email' => $this->faker->unique->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'country_id' => Country::factory()->create(),
+            'company_name' => $this->faker->company(),
+            'website' => $this->faker->domainName(),
             'status' => true,
         ];
     }
