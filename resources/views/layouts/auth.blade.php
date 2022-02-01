35cfda6617e80b3bbf01bdc8bf126a705d89bf5d
jabber <2898117012@qq.com>
2022-02-01 17:25:05 +0800

    fix: title

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8"/>
-    <title>@yield('title') | {{ __('seller::auth.title') }} - {{ config('app.name') }}</title>
+    <title>@yield('title') - {{ __('seller::auth.title') }} - {{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
