3d2d9c560e32eafe3cbc52ff691002feaad7d130
jabber <2898117012@qq.com>
2022-02-14 15:18:00 +0800

    fix: test

--- src/Tests/TestCase.php
+++ src/Tests/TestCase.php
@@ -2,27 +2,11 @@
 
 namespace Dealskoo\Seller\Tests;
 
-use Dealskoo\Seller\Facades\SellerMenu;
 use Dealskoo\Seller\Models\Seller;
-use Dealskoo\Seller\Providers\SellerServiceProvider;
 use Dealskoo\Seller\Tests\Http\Kernel;
 
 abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            SellerServiceProvider::class
-        ];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'SellerMenu' => SellerMenu::class
-        ];
-    }
-
     protected function getEnvironmentSetUp($app)
     {
         parent::getEnvironmentSetUp($app);
@@ -46,10 +30,5 @@ abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
     protected function resolveApplicationHttpKernel($app)
     {
         $app->singleton(\Illuminate\Contracts\Http\Kernel::class, Kernel::class);
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
     }
 }
